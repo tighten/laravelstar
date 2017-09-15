f8bea17bf93ea20016d264daf09597f893d80244
Matt Stauffer <matt@tighten.co>
2017-09-14 22:08:00 -0400

    Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Post;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $user1 = factory(User::class)->create();
+
+        $post1 = factory(Post::class)->create();
+        $post2 = factory(Post::class)->create();
+        $post3 = factory(Post::class)->create();
+
+        $user1->submit($post1);
+        $user1->submit($post2);
+        $user1->submit($post3);
+
+        $user2 = factory(User::class)->create();
+
+        $post4 = factory(Post::class)->create();
+
+        $user2->submit($post3);
+        $user2->submit($post4);
+
+        $user1->like($post4);
+        $user1->read($post4);
+
+        $user2->like($post1);
+        $user2->read($post2);
     }
 }
--- features.md
+++ features.md
@@ -4,3 +4,11 @@
 - Admins can set up sources as to-be-pulled
 - Single stream output of all posts
 - Some way to join authors manually and also maybe smart automatically so Matt Stauffer the blog author is connected to Matt Stauffer the twitter user
+
+# Tasks
+- [x] Add tests and functionality around posts and submissions
+- [x] Add tests and functionality around reads and likes
+- [ ] Add tests and functionality around basic post list pages and submission UI
+- [ ] Basic user signup
+- [ ] Basic user profiles
+- [ ] Add tests and functionality around authors
